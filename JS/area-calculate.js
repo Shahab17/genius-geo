9e574f5528062edf82cae5429ab18929d63246ec
ShahabMat <cse_1812020090@lus.ac.bd>
2023-08-18 16:54:18 +0600

    first commit

---
+++ JS/area-calculate.js
@@ -0,0 +1,35 @@
+function triangleCalculate(){
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const baseValue = parseFloat(baseValueText);
+    
+    // height field 
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const heightValue = parseFloat(heightValueText);
+   
+    // show area
+    const area = 0.5 * baseValue * heightValue;
+     
+    const areaShow = document.getElementById('area-show');
+    areaShow.innerText = area;   
+}
+
+function rectangleCalculate(){
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const widthValue = parseFloat(widthValueText);
+    
+    // length value 
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value ;
+    const lengthValue = parseFloat(lengthValueText);
+    console.log(widthValue, lengthValue)
+
+    // show area
+    const area = widthValue * lengthValue;
+
+    const areaShow = document.getElementById('rec-area-show');
+    areaShow.innerText = area;
+     
+}
